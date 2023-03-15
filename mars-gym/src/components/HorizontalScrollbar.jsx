99a84a605d6895419b075652170f2899e4866b98
Olavo Mars <olavoamneves@gmail.com>
2023-03-14 23:06:28 -0400

    adding horizontal scrollbar and Body Part component

---
+++ mars-gym/src/components/HorizontalScrollbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Box} from '@mui/material';
+import {ScrollMenu, VisibilityContext} from 'react-horizontal-scrolling-menu';
+
+import BodyPart from './BodyPart';
+
+const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
+  return (
+    <ScrollMenu>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemID={item.id || item}
+          title={item.id || item}
+          m='0 30px'
+        >
+          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+};
+
+export default HorizontalScrollbar;
--- mars-gym/src/components/SearchExercises.jsx
+++ mars-gym/src/components/SearchExercises.jsx
@@ -2,9 +2,11 @@ import React, {useEffect, useState} from 'react';
 import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 import {exerciseOptions, fetchData} from '../utils/fetchData';
 
-const SearchExercises = () => {
+import HorizontalScrollbar from './HorizontalScrollbar';
+
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
   const [search, setSearch] = useState('');
-  const [exercises, setExercises] = useState([]);
+
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
@@ -79,7 +81,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
-        {/* <HorizontalScrollBar data={bodyParts} /> */}
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
