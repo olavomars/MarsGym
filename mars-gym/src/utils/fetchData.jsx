04b6852850c4b22edb35cc81d923d770cfdb2ec9
Olavo Mars <olavoamneves@gmail.com>
2023-03-16 17:51:08 -0400

    updating environmental key

--- mars-gym/src/utils/fetchData.jsx
+++ mars-gym/src/utils/fetchData.jsx
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.VITE_REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
