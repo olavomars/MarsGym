a56eab9412896f43b4c045008e25e9cbff7da0e9
Olavo Mars <olavoamneves@gmail.com>
2023-03-16 18:15:29 -0400

    fixing css responsiveness on video cards

--- mars-gym/src/components/SimilarExercises.jsx
+++ mars-gym/src/components/SimilarExercises.jsx
@@ -7,7 +7,7 @@ import Loader from './Loader';
 const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) => {
   return (
     <Box sx={{mt: {lg: '100px', xs: '0'}}}>
-      <Typography variant='h4' mb={5}>
+      <Typography variant='h4' mb={5} mt={4}>
         Exercises that target the same muscle group
       </Typography>
       <Stack direction='row' styles={{p: '2', position: 'relative'}}>
--- mars-gym/src/utils/fetchData.jsx
+++ mars-gym/src/utils/fetchData.jsx
@@ -1,6 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
+    // 'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Key': process.env.VITE_REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
@@ -9,6 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
+    // 'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Key': process.env.VITE_REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
   },
