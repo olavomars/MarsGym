049d76c3c5731411613312c70098a65e2203bd2f
Olavo Mars <olavoamneves@gmail.com>
2023-03-16 17:52:33 -0400

    updating other environmental key

--- mars-gym/src/utils/fetchData.jsx
+++ mars-gym/src/utils/fetchData.jsx
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.VITE_REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
   },
 };
