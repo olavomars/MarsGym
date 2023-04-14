b672f000c64b70695e3bd7734ea4c2505c4f2d57
Olavo Mars <olavoamneves@gmail.com>
2023-04-13 20:20:59 -0400

    commiting with alternative environment key in build

--- mars-gym/src/utils/fetchData.jsx
+++ mars-gym/src/utils/fetchData.jsx
@@ -1,16 +1,16 @@
-export const youtubeOptions = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-  },
-};
-
 export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
   },
 };
 
